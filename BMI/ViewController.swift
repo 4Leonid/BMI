e0aa459407bffa9f76900407e511104be7db28a8
Leonid <leo-turko@mail.ru>
2023-04-15 19:16:01 +0300

    Initial Commit

---
+++ BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Леонид Турко on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
